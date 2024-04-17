9c2ad04154477c79792fc179f1635162a69bb5f6
unknown <leonideko1@gmail.com>
2024-04-17 19:02:01 +0300

    add to github

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import { v4 as uuidv4 } from "uuid";
+import List from "./components/List";
+//*className='container', "input-filed" - це клас з матеріалайз
 
 function App() {
+  // const todotask = [{ id: 0, title: "Clean", status: false }];
+  const [tasks, setTasks] = useState(() => {
+    const storedTodos = localStorage.getItem("tasks");
+    if (!storedTodos) {
+      return [];
+    } else {
+      return JSON.parse(storedTodos);
+    }
+  });
+  const [taskTitle, setTasksTitle] = useState("");
+
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
+
+  const addTask = (e) => {
+    const storedTodos = JSON.parse(localStorage.getItem("tasks"));
+    if (e.key === "Enter" && e.target.value !== "") {
+      setTasks([
+        //...tasks - було
+        ...storedTodos,
+        {
+          id: uuidv4(),
+          title: taskTitle,
+          status: false,
+        },
+      ]);
+      setTasksTitle("");
+    }
+  };
+
+  const date = new Date();
+  const monthNames = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  const month = monthNames[date.getMonth()];
+  const day = date.getDate();
+  const year = date.getFullYear();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Note your tasks</h1>
+      <span>{month + " " + day + ", " + year}</span>
+      <div className="input-filed">
+        <input
+          type="text"
+          value={taskTitle}
+          onChange={(event) => setTasksTitle(event.target.value)}
+          onKeyDown={addTask}
+        />
+        <label>Write your task name and press ENTER</label>
+      </div>
+      <List tasks={tasks} />
     </div>
   );
 }
